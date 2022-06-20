a414d4c6b015962afe612ede293d590decf70bfb
fkolano <fkolano@gmail.com>
2022-06-20 14:50:41 +0000

    final commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
 import { TrafficLight } from "./trafficlight.jsx";
 
-
-
-
 const Home = () => {
-	return (
-		<div>
-			<TrafficLight />,
-			
-		</div>
-	);
+  return (
+    <div>
+      <TrafficLight />,
+    </div>
+  );
 };
 
 export default Home;
